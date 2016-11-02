0f2cbd2b8e7b84f24c29a0bad4e26fb178453d21
Hana Sinishaw <hana.sinishaw@gmail.com>
2016-11-02 12:35:57 +0300

    modify json parsing

--- external-tools/glimpse/js/directives/graph.js
+++ external-tools/glimpse/js/directives/graph.js
@@ -9,46 +9,27 @@ angular.module('glimpse')
       
             var query_string = response.data.response;
             console.log("query_stringis"+query_string)
-            scope.arrString1 = new Array();
-            scope.arrString1 = query_string.split(",");
-            scope.chartSeries = [];
-            console.log("array length is" + scope.arrString1.length);
-            scope.AllEmoVariablesName = [];
-            scope.AllEmoVariablesValue = [];
-               for(var i =0 ; i < scope.arrString1.length; i++){
-                   
-                    scope.arrString2 = new Array();
-                    var arrtoString = scope.arrString1[i].toString();
-                    scope.arrString2 = arrtoString.split(":");
-                    scope.StringPairName = scope.arrString2[0];
-                    scope.StringPairValue = scope.arrString2[1];
-                  var inputarrry = [];
-                  inputarrry = [0.5];
-                    for(var j = 1; j< scope.arrString2.length; j++){
-                        scope.AllEmoVariablesName.push(scope.StringPairName);
-                        console.log("NAME ARRAY" + scope.AllEmoVariablesName );
-                        scope.AllEmoVariablesValue.push(scope.StringPairValue);
-                        console.log("VALUE ARRAY" + scope.AllEmoVariablesValue );
-                        console.log("scope.arrString2[0]" + scope.arrString2[0] );
-                        console.log("scope.arrString2[1]" + scope.arrString2[1] );
-                        
-                         //   scope.chartSeries.push({"name": scope.arrString2[0]}, 
+        
 
+            scope.results = JSON.parse(query_string);
+            console.log("scope.results"+ scope.results);
+            console.log("results: ");
+            for (varname in scope.results) {
+              console.log("    " + varname + ": " + scope.results[varname])
 
-                      // {"data": scope.arrString2[1]});
+                var key=varname;
+                var value= scope.results[varname];
 
-                          // console.log("scope.chartSeries" + scope.chartSeries );
-                          //  console.log("scope.chartSeries[0].name" + scope.chartSeries[0].name );
-                          //  console.log("scope.chartSeries[0].data" + scope.chartSeries[1].data );
+          }
+                console.log(JSON.stringify(value));
+                console.log(key);
 
-
-                    }
-               }
-
-               scope.chartSeries = [
-                  {"name": scope.AllEmoVariablesName, "data": scope.AllEmoVariablesValue},
-                  {"name": "Some data 3", "data": [3, 1, null, 5, 2], connectNulls: true}
-
+                var arraysv = [];
+                arraysv.push(value);
+                console.log("arraysv" + arraysv);
+                scope.chartSeries = [
+                  {"name": key, "data": arraysv}
+                 
                 ];
 
                 scope.chartConfig = {
@@ -72,21 +53,7 @@ angular.module('glimpse')
                       loading: false,
                       size: {}
                     }
-                                      //scope.poll = function() {
-                                //               $timeout(function(){
-                                //                  
-                                //                   scope.chartConfig.series[0].data.shift();
-                                //                   scope.chartConfig.series[0].data.push(scope.AllEmoVariablesName);
-                                //                   // scope.chartConfig.series[1].data.shift();
-                                //                   // scope.chartConfig.series[1].data.push(Math.floor(Math.random() * 20) + 1);
-                                //                   scope.poll();
-                                //               }, 2000);
-                                //           }
-
-                                // this.$onInit = function() {
-                                //     console.log("SUCCESSFULLY UPDATED SERIES");
-                                //     scope.poll();
-                                // }
+                             
         }, function errorCallback(err) {
               console.log(err.message)
               
